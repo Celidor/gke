c82e0a11c9becbf3658b5060e5914cd68ea276c9
Paul Schwarzenberger <paul@celidor.net>
2021-03-29 10:44:15 +0100

    update to enable clean terraform destroy

--- services.tf
+++ services.tf
@@ -1,5 +1,6 @@
 resource "google_project_service" "enabled_apis" {
-  project  = var.project
-  for_each = toset(local.service_apis)
-  service  = each.value
+  project                    = var.project
+  for_each                   = toset(local.service_apis)
+  service                    = each.value
+  disable_dependent_services = true
 }
